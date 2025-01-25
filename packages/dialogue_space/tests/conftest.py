aef8c417f52d3989bc42b2e5dc86838ecac8456a
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-01-25 16:23:31 +0000

    chore(pytest): use importlib and fix other test files
    This appears to fix the gripes I was having with setting up tests in the past. Removing the pytest_plugins from the thing means that theres no dedicated fixtures folder for the entire thing, but we dont _need_ that

--- packages/dialogue_space/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-# Import all the fixtures from every file in the tests/fixtures dir.
-pytest_plugins = [
-    fixture_file.as_posix().replace("/", ".").replace(".py", "")
-    for fixture_file in Path().rglob("tests/fixtures/**/[!__]*.py")
-]
