c9126f2e46912119c0ef2724a6ab8a12f5ab0275
Godugu Anil Himam <63832213+anilhimam17@users.noreply.github.com>
2025-04-08 16:25:50 +0100

    feat(gradio/defuser): add discrete actions
    * feat: added discrete actions to defuser.py
    
    * refactor(gradio/defuser): remove unneeded args for button

--- src/gptnt/app/views/defuser.py
+++ src/gptnt/app/views/defuser.py
@@ -1,3 +1,4 @@
+from functools import partial
 from pathlib import Path
 from typing import override
 
@@ -26,18 +27,51 @@ class DefuserPlayerView(BasePlayerView):
     def render_viewing_window(self) -> None:
         """Create video feed for defuser."""
         with gr.Column(scale=2):
+            # Build the game viewing window
             image_html = f"""
-            <img src="{self.endpoint}" width="100%" height="750" style="object-fit: cover;">
-            """
+                <img src="{self.endpoint}" width="100%" height="750" style="object-fit: cover;">
+                """
             self._game_window = gr.HTML(image_html, elem_id="video_feed")
             output = gr.Textbox(label="Response from Python", elem_id="real_box", visible=False)
             _ = output.input(fn=self._handle_textbox_change, inputs=[output], outputs=None)
+
+            # Discrete Action Buttons
+            with gr.Column(scale=3):
+                with gr.Row():
+                    self._button_left = gr.Button("Turn Left")
+                    self._button_right = gr.Button("Turn Right")
+                    self._button_around = gr.Button("Turn Around")
+
+                with gr.Row():
+                    self._button_roll_up = gr.Button("Roll Up")
+                    self._button_roll_down = gr.Button("Roll Down")
+                    self._button_zoom_out = gr.Button("Zoom Out")
+
+        # Registering the actions
+        _ = self._button_left.click(
+            fn=partial(self._handle_discrete_action, GameActionType.turn_left)
+        )
+        _ = self._button_right.click(
+            fn=partial(self._handle_discrete_action, GameActionType.turn_right)
+        )
+        _ = self._button_around.click(
+            fn=partial(self._handle_discrete_action, GameActionType.turn_around)
+        )
+        _ = self._button_roll_up.click(
+            fn=partial(self._handle_discrete_action, GameActionType.roll_up)
+        )
+        _ = self._button_roll_down.click(
+            fn=partial(self._handle_discrete_action, GameActionType.roll_down)
+        )
+        _ = self._button_zoom_out.click(
+            fn=partial(self._handle_discrete_action, GameActionType.zoom_out)
+        )
 
     @override
     def load_custom_js(self) -> str:
         return Path(__file__).parent.joinpath("defuser_script.js").read_text()
 
-    def _handle_textbox_change(self, text: str) -> None:
+    async def _handle_textbox_change(self, text: str) -> None:
         # Parse JS mouse events and send to KTANE client
         logger.info(text)
         match text.split(sep=" "):
@@ -47,7 +81,7 @@ class DefuserPlayerView(BasePlayerView):
                     action=GameActionType.hold,
                     location=RelativeCoordinate(x_pos=float(event_x), y_pos=float(event_y)),
                 )
-                _ = self.ktane_client.send_action(action)
+                _ = await self.ktane_client.send_action(action)
                 self.previous_event = "Mousedown"
 
             case ["Mouseup", _, "X:", event_x, "Y:", event_y, _, _]:
@@ -61,9 +95,15 @@ class DefuserPlayerView(BasePlayerView):
                         location=RelativeCoordinate(x_pos=float(event_x), y_pos=float(event_y)),
                     )
 
-                _ = self.ktane_client.send_action(action)
+                _ = await self.ktane_client.send_action(action)
                 self.previous_event = "Mouseup"
 
             # Incorrect event (throw error?)
             case err:
                 logger.error(f"Invalid  mouse-event format recieved from JS: {err}")
+
+    async def _handle_discrete_action(self, action_type: GameActionType) -> None:
+        """Send the discrete action to the KTANE Client."""
+        logger.debug(action_type)
+        action = KtaneAction(action=action_type)
+        _ = await self.ktane_client.send_action(action)
