dc0849a1d95b050b970526979cebb813c1a4648f
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-04-30 16:07:31 +0100

    test(players/ai): mock return from ktane client to have actual images

--- tests/players/conftest.py
+++ tests/players/conftest.py
@@ -1,8 +1,13 @@
+from io import BytesIO
+from pathlib import Path
+
 import httpx
 import pytest
+from PIL.Image import Resampling
 from pytest_mock import MockerFixture
 from whenever import Instant
 
+from gptnt.common.image_ops import load_observation_from_bytes
 from gptnt.dialogue_space.client import DialogueSpaceClient
 from gptnt.ktane.client import KtaneClient
 from gptnt.ktane.state.bomb import BombState
@@ -16,13 +21,33 @@ def dialogue_space_client(mocker: MockerFixture) -> DialogueSpaceClient:
     return client
 
 
+@pytest.fixture(scope="session")
+def images(fixture_path: Path) -> tuple[bytes, bytes]:
+    """Fixture to provide a segmentation screenshot and a screenshot as numpy arrays."""
+    segmentation_image_path = fixture_path.joinpath("segmentation1.png")
+    screenshot_image_path = fixture_path.joinpath("screenshot1.png")
+
+    segmentation_image = load_observation_from_bytes(segmentation_image_path.read_bytes())
+    screenshot_image = load_observation_from_bytes(screenshot_image_path.read_bytes())
+
+    # make the iamge smaller for testing
+    segmentation_image = segmentation_image.resize((512, 512), resample=Resampling.NEAREST)
+    screenshot_image = screenshot_image.resize((512, 512), resample=Resampling.NEAREST)
+
+    screenshot_buffer = BytesIO()
+    screenshot_image.save(screenshot_buffer, format="PNG")
+    segmentation_buffer = BytesIO()
+    segmentation_image.save(segmentation_buffer, format="PNG")
+    return screenshot_buffer.getvalue(), segmentation_buffer.getvalue()
+
+
 @pytest.fixture
-def game_client(mocker: MockerFixture) -> KtaneClient:
+def game_client(mocker: MockerFixture, images: tuple[bytes, bytes]) -> KtaneClient:
     base_url = "http://localhost:1"
 
     game_client = KtaneClient(client=httpx.AsyncClient(base_url=base_url))
     game_client.get_observation = mocker.AsyncMock()
-    game_client.get_observation.return_value = b"", b"", b""
+    game_client.get_observation.return_value = images[0], images[1], images[0]
     game_client.get_state = mocker.AsyncMock()
     game_client.get_state.return_value = BombState.model_validate(
         {
