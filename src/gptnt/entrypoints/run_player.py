db9cfc0447f217779214cd71dc7dcb9a73c07da3
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-05-29 16:14:40 +0100

    fix(rabbitmq): raise exceptions when the player handles from rabbit

--- src/gptnt/entrypoints/run_player.py
+++ src/gptnt/entrypoints/run_player.py
@@ -1,10 +1,12 @@
 import asyncio
 import sys
-from typing import TYPE_CHECKING
 
 import hydra
 import logfire
+from faststream import ExceptionMiddleware
 from faststream.app import FastStream
+from faststream.rabbit import RabbitBroker
+from faststream.rabbit.opentelemetry import RabbitTelemetryMiddleware
 from opentelemetry.sdk.trace.sampling import ParentBased
 from structlog import get_logger
 
@@ -14,10 +16,6 @@ from gptnt.common.paths import Paths
 from gptnt.common.prompt_cache import PromptCache
 from gptnt.ktane.manual import KtaneManualPaths
 from gptnt.players.base_player import BasePlayer
-
-if TYPE_CHECKING:
-    from gptnt.players.ai_player import AIPlayer
-
 
 _ = logfire.configure(
     service_name="player",
@@ -31,6 +29,15 @@ _logger = get_logger()
 paths = Paths()
 ktane_manual_paths = KtaneManualPaths()
 
+exc_middleware = ExceptionMiddleware()
+
+
+@exc_middleware.add_handler(Exception, publish=True)
+def error_handler(exc: Exception) -> None:
+    """Handle exceptions raised in the player from handlers."""
+    _logger.exception("An error occurred in the player", exc_info=exc)
+    sys.exit(1)
+
 
 async def run_player(*, hydra_overrides: list[str]) -> None:
     """Run the player."""
@@ -40,7 +47,11 @@ async def run_player(*, hydra_overrides: list[str]) -> None:
     with hydra.initialize_config_dir(version_base="1.3", config_dir=str(paths.configs)):
         config = hydra.compose(config_name="player.yaml", overrides=hydra_overrides)
     # Instantiate the player from the class
-    player: AIPlayer = hydra.utils.instantiate(config.player)
+    player_partial = hydra.utils.instantiate(config.player)
+
+    broker = RabbitBroker(logger=None, middlewares=(RabbitTelemetryMiddleware(), exc_middleware))
+    player_partial.keywords["broker"] = broker
+    player = player_partial()
     assert isinstance(player, BasePlayer)
 
     app = FastStream(player.broker, lifespan=player.lifespan, logger=None)
