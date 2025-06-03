ffe3396ca6e476c517269c73e3f5b41c10a96060
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-06-03 12:00:35 +0100

    feat: use typer to run evaluation in one file

--- src/gptnt/entrypoints/run_evaluation.py
+++ src/gptnt/entrypoints/run_evaluation.py
@@ -1,37 +1,82 @@
-import sys
+from typing import Annotated
 
 import hydra
+import typer
 from pydantic_ai import Agent
 from structlog import get_logger
 
 from gptnt.common.logger import configure_logging
 from gptnt.common.paths import Paths
-from gptnt.dataset.evaluation_logger import throw_weave_eval
+from gptnt.dataset.evaluation_logger import send_to_weave, throw_weave_eval
 
 configure_logging()
 logger = get_logger()
 paths = Paths()
 
 
-def load_agent_from_hydra(*, hydra_overrides: list[str]) -> Agent:
-    """Load the agent from the Hydra config."""
+app = typer.Typer()
+
+
+ModelOption = Annotated[str, typer.Option(help="The model to use.")]
+ThrowOption = Annotated[bool, typer.Option("--throw", help="Should we throw the evaluation?")]
+UploadOption = Annotated[
+    bool, typer.Option("--upload", help="Should we upload the evaluation results?")
+]
+
+
+def load_agent(model: str) -> Agent:
+    """Load the agent from the Hydra config with the given model."""
     with hydra.initialize_config_dir(version_base="1.3", config_dir=str(paths.configs)):
-        config = hydra.compose(config_name="player", overrides=hydra_overrides)
+        config = hydra.compose(config_name="player", overrides=[f"model={model}"])
     # Instantiate the agent from the class
     agent: Agent = hydra.utils.instantiate(config.player.agent)
     assert isinstance(agent, Agent), "The instantiated agent is not an Agent."
     return agent
 
 
-def run_evaluation(agent: Agent) -> None:
-    """Run the evaluation with the given agent."""
+@app.command("grounding")
+def run_grounding_evaluation(
+    *, model: ModelOption, should_throw: ThrowOption = False, should_upload: UploadOption = False
+) -> None:
+    """Run the defuser grounding evaluation."""
+    agent = load_agent(model)
     logger.info("Running evaluation", agent=agent)
-    throw_weave_eval(name="grounding", agent=agent, hf_dataset="GPTNT/grounding-dataset")
+
+    if should_throw:
+        throw_weave_eval(name="grounding", agent=agent, hf_dataset="GPTNT/grounding-dataset")
+    if should_upload:
+        send_to_weave(name="grounding", agent=agent, hf_dataset="GPTNT/grounding-dataset")
+
+    logger.info("Evaluation completed successfully", agent=agent)
+
+
+@app.command("vqa")
+def run_vqa_evaluation(
+    *, model: ModelOption, should_throw: ThrowOption = False, should_upload: UploadOption = False
+) -> None:
+    """Run the defuser VQA."""
+    agent = load_agent(model)
+    logger.info("Running VQA evaluation", agent=agent)
+    if should_throw:
+        raise NotImplementedError
+    if should_upload:
+        raise NotImplementedError
+    logger.info("VQA evaluation completed successfully", agent=agent)
+
+
+@app.command("expert-vqa")
+def run_expert_vqa_evaluation(
+    *, model: ModelOption, should_throw: ThrowOption = False, should_upload: UploadOption = False
+) -> None:
+    """Run the expert VQA evaluation."""
+    agent = load_agent(model)
+    logger.info("Running expert VQA evaluation", agent=agent)
+    if should_throw:
+        raise NotImplementedError
+    if should_upload:
+        raise NotImplementedError
+    logger.info("Expert VQA evaluation completed successfully", agent=agent)
 
 
 if __name__ == "__main__":
-    hydra_overrides = sys.argv[1:] if len(sys.argv) > 1 else []
-    if hydra_overrides:
-        logger.debug(f"Hydra overrides: {hydra_overrides}")
-    agent = load_agent_from_hydra(hydra_overrides=hydra_overrides)
-    run_evaluation(agent)
+    app()
