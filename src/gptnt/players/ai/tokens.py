fb31711d5bc1845c0ce75d35d7b6e0864eb772f6
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-10-06 10:33:10 -0700

    chore: add todos to the token estimation calc

--- src/gptnt/players/ai/tokens.py
+++ src/gptnt/players/ai/tokens.py
@@ -6,24 +6,25 @@ from tiktoken.model import MODEL_PREFIX_TO_ENCODING
 
 TIKTOKEN_ENCODING_NAME = MODEL_PREFIX_TO_ENCODING["gpt-4o-"]
 
-DEFAULT_IMAGE_WIDTH = 512
-DEFAULT_IMAGE_HEIGHT = 512
-
 
 def estimate_tokens_for_image_per_model(model: str, *, width: int, height: int) -> int:  # noqa: WPS212
     """Get the computation function for the model."""
+    model = model.lower()
     if "claude" in model:
         return math.ceil(width * height / 750)
     if "gemini" in model:
+        # TODO: Recompute for the new image size
         return 1290
+    # TODO: support o4-mini for the model name
+    # TODO: Check what o4-mini's tokens per image is
     if "gpt4o" in model or "gpt-4o" in model:
         return 85
-    if "qwen" in model.lower():
+    if "qwen" in model:
         # resized to 504x504 (multiples of 28)
         # patch size is 14x14 - 2x2 token merging
         # (504 / 14)^2 / 2^2 = 324
         return 324
-    if "internvl3" in model.lower():
+    if "internvl3" in model:
         # resized to 448x448 (multiples of 448)
         # patch size is 14x14 - pixel unshuffle == 2x2 token reduction
         # (448 / 14)^2 / 2^2 = 256
