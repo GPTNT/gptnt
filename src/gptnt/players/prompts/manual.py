04f123a5a5bef2c58de8cfd4ba7b3c2c0d1d229a
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-10-13 11:16:04 -0700

    feat: restrict manual pages further
    feat: restrict which pages from the manual we use further
    
    remove pages 1,2,4 aswell and include an option to skip the appendix pges if we decide to

--- src/gptnt/players/prompts/manual.py
+++ src/gptnt/players/prompts/manual.py
@@ -3,23 +3,39 @@ from functools import lru_cache
 import structlog
 from pydantic_ai import BinaryContent
 
-from gptnt.ktane.manual import MANUAL_NUM_PAGES, NEEDY_MODULE_PAGE_NUMS, KtaneManualPaths
+from gptnt.ktane.manual import (
+    APPENDIX_PAGES,
+    EXPLAINER_PAGES_TO_REMOVE,
+    MANUAL_NUM_PAGES,
+    NEEDY_MODULE_PAGE_NUMS,
+    KtaneManualPaths,
+)
 
 logger = structlog.get_logger()
 
 
 @lru_cache(maxsize=1)
 def load_manual_as_prompt(
-    *, num_pages: int = MANUAL_NUM_PAGES, skip_needy_modules: bool = True
+    *,
+    num_pages: int = MANUAL_NUM_PAGES,
+    skip_needy_modules: bool = True,
+    skip_explainer_pages: bool = True,
+    skip_appendix_pages: bool = False,
 ) -> list[str | BinaryContent]:
     """Load the content for the manual."""
     logger.debug(f"Loading {num_pages} pages of the manual")
     manual_paths = KtaneManualPaths()
 
+    pages_to_skip: list[int] = [
+        *(NEEDY_MODULE_PAGE_NUMS if skip_needy_modules else []),
+        *(EXPLAINER_PAGES_TO_REMOVE if skip_explainer_pages else []),
+        *(APPENDIX_PAGES if skip_appendix_pages else []),
+    ]
+
     manual = []
     for page_num in range(1, num_pages + 1):
-        if skip_needy_modules and page_num in NEEDY_MODULE_PAGE_NUMS:
-            # Skip the needy module pages
+        if page_num in pages_to_skip:
+            logger.debug(f"Skipping page {page_num} of the manual")
             continue
 
         # Load the text for the page first
