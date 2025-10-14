73d4f8c77e1f167cc52849f532c361d9ad92e7cd
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2025-10-13 19:58:51 -0700

    refactor: simplify complex nested func
    * refactor: simplify complex nested func
    
    * refactor: be more concise and efficient

--- src/gptnt/common/typing_ops.py
+++ src/gptnt/common/typing_ops.py
@@ -4,23 +4,16 @@ from typing import Annotated, Union, get_args, get_origin
 
 def extract_base_types(type_hint: type) -> set[type]:
     """Recursively extract all base types from a nested Union/Annotated structure."""
-    base_types = set()
+    origin = get_origin(type_hint)
+    args = get_args(type_hint)
 
-    def _extract_recursive(current_type: type) -> None:  # noqa: WPS430
-        origin = get_origin(current_type)
+    # Handle Union types (both Union[A, B] and A | B syntax)
+    if origin in (Union, getattr(types, "UnionType", None)):
+        return {base_type for arg in args for base_type in extract_base_types(arg)}
 
-        # Handle Union types (both Union[A, B] and A | B syntax)
-        if origin is Union or (hasattr(types, "UnionType") and origin is types.UnionType):
-            # It's a Union, process each member
-            for arg in get_args(current_type):
-                _extract_recursive(arg)
-        elif origin is Annotated:
-            # It's Annotated, get the first argument (the actual type)
-            actual_type = get_args(current_type)[0]
-            _extract_recursive(actual_type)
-        else:
-            # It's a base type (or something we don't need to recurse into)
-            base_types.add(current_type)
+    # Handle Annotated types
+    if origin is Annotated:
+        return extract_base_types(args[0])
 
-    _extract_recursive(type_hint)
-    return base_types
+    # Base case: it's a base type
+    return {type_hint}
