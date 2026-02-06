90cf9d9966d1918e7ec9a5d160654006288bc30b
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2026-02-06 13:44:08 +0000

    fix(statics): resizing arrays were the wrong way around

--- src/gptnt/processors/image_resizer.py
+++ src/gptnt/processors/image_resizer.py
@@ -38,7 +38,7 @@ class ImageResizer:
     def resize_array(self, array: NDArray[Any]) -> NDArray[Any]:
         """Resize a numpy array representing an image to target specifications."""
         return cv2.resize(
-            array, (self.target_height, self.target_width), interpolation=self.resampling_method
+            array, (self.target_width, self.target_height), interpolation=self.resampling_method
         )
 
     def convert_absolute_to_relative(
